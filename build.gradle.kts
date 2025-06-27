4a5237b3cc73f9414d40879926eb8f97aacd5335
Renji Yuusei <daoluc.yy@gmail.com>
2025-06-27 12:32:49 +0700

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     id("com.android.application") apply false
-    alias(libs.kotlin.android.plugin) apply false
-    alias(libs.kotlin.jvm.plugin) apply false
-    alias(libs.kotlin.serialization.plugin) apply false
+    alias(libs.plugins.jetbrains.kotlin.android) apply false
+    alias(libs.plugins.jetbrains.kotlin.jvm) apply false
+    alias(libs.plugins.kotlin.serialization) apply false
 }
