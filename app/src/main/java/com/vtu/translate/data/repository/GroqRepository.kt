89658a25a024e962445939556512aa3b73313b4a
Renji Yuusei <daoluc.yy@gmail.com>
2025-06-28 19:58:54 +0700

    build: add experimental serialization API opt-in
    Enable kotlinx.serialization experimental API to support new serialization features in GroqRepository

--- app/src/main/java/com/vtu/translate/data/repository/GroqRepository.kt
+++ app/src/main/java/com/vtu/translate/data/repository/GroqRepository.kt
@@ -6,6 +6,7 @@ import com.vtu.translate.data.model.ChatCompletionResponse
 import com.vtu.translate.data.model.ChatMessage
 import com.vtu.translate.data.model.GroqModelsResponse
 import kotlinx.coroutines.flow.first
+import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.json.Json
 import okhttp3.MediaType.Companion.toMediaType
 import okhttp3.OkHttpClient
@@ -40,6 +41,7 @@ class GroqRepository(private val preferencesRepository: PreferencesRepository) {
         .addInterceptor(loggingInterceptor)
         .build()
     
+    @OptIn(ExperimentalSerializationApi::class)
     private val retrofit = Retrofit.Builder()
         .baseUrl(BASE_URL)
         .client(okHttpClient)
