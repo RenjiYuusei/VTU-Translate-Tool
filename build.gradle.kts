65f8b2e25f0eeb2075b9f6ce0a5240e01ae6bf5f
Renji Yuusei <daoluc.yy@gmail.com>
2025-06-28 11:48:56 +0700

    Refactor build.gradle.kts to streamline task configuration and remove unnecessary buildscript sections
    - Removed the buildscript block and its dependencies to simplify the Gradle configuration.
    - Updated the clean task to use layout.buildDirectory for improved clarity and maintainability.

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,24 +5,6 @@ plugins {
     id("org.jetbrains.kotlin.plugin.serialization") version "1.9.22" apply false
 }
 
-buildscript {
-    repositories {
-        google()
-        mavenCentral()
-    }
-    dependencies {
-        classpath("com.android.tools.build:gradle:8.3.2")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.22")
-    }
-}
-
-allprojects {
-    repositories {
-        google()
-        mavenCentral()
-    }
-}
-
 tasks.register("clean", Delete::class) {
-    delete(rootProject.buildDir)
+    delete(layout.buildDirectory)
 } 
